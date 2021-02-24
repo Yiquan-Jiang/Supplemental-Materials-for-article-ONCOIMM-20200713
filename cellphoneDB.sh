4581c24350051651980124a3067ecd1839f53f9d
Yi-Quan Jiang <46735478+Yiquan-Jiang@users.noreply.github.com>
2021-02-24 16:34:42 +0800

    Add files via upload

---
+++ cellphoneDB.sh
@@ -0,0 +1,10 @@
+cellphonedb method statistical_analysis \
+       --project-name tissue \
+       --iterations 1000 \
+       --threshold 0.2 \
+       --result-precision 3 \
+       --output-path /ObsPath/Cells_Communications \
+       --verbose \
+       --threads 16 \
+       /ObsPath/Cell_type.txt \
+       /ObsPath/Expression.txt 
